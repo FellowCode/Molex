47fcfd201da0e4a205b5ef785628dfc36f899051
FellowCode <31797522+FellowCode@users.noreply.github.com>
2019-01-18 10:28:44 +1000

    delete about v2

--- Main/admin.py
+++ Main/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from .models import CarouselImage, ServiceOrder, Budget
+from .models import CarouselImage, Budget
 
 admin.site.register(CarouselImage)
-
-@admin.register(ServiceOrder)
-class ServiceOrderAdmin(admin.ModelAdmin):
-    readonly_fields = ['service_type', 'email', 'name', 'phone', 'note']
 
 admin.site.register(Budget)
--- Main/urls.py
+++ Main/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from Main.views import Index, About, ServiceOrderView, ServiceOrderAccept, Support, SupportAccept
+from Main.views import Index, About, Support, SupportAccept
 
 urlpatterns = [
     path('about/', About),
     path('support/accept/', SupportAccept),
     path('support/', Support),
-    path('serviceorder/accept/', ServiceOrderAccept),
-    path('serviceorder/', ServiceOrderView),
     path('', Index),
 ]
 
